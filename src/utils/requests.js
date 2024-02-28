d856ae020ab57c3d8556f65c89527c155c136450
Serg <serega101617@gmail.com>
2024-02-28 16:45:27 +0100

    fix weather reference

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -3,7 +3,7 @@ const API_KEY = '694807e56a256cbe52bf258e4abb7229';
 export const fetchWeather = async (setWeatherData, city = 'Karlsruhe') => {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`
     );
     if (!response.ok) {
       throw new Error(`HTTP error! Status: ${response.status}`);
